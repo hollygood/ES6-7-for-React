369a468c7f639722721726ea785b1cbc8f2e11b5
Kathy H <kathykehuang@gmail.com>
2016-07-29 17:06:59 -0400

    add routes

--- app/index.js
+++ app/index.js
@@ -1,51 +1,9 @@
-var USER_DATA = {
-	name: 'Kathy Huang',
-	username: 'kathyhuang',
-	image: 'https://design.atlassian.com/images/avatars/avatar-96.png'
-};
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('../config/routes');
 
-var ProfilePic = React.createClass({
-	render: function () {
-		return <img src={this.props.imageUrl} />
-	}
-});
-
-var ProfileLink = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<a href={'https://github.com/' + this.props.username}>
-					{this.props.username}
-				</a>
-			</div>
-		)
-	}
-});
-
-var ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>{this.props.name}</div>
-		)
-	}
-});
-
-var Avatar = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image}/>
-				<ProfileName name={this.props.user.name}/>
-				<ProfileLink username={this.props.user.username} />
-			</div>
-		)
-	}
-});
 
 ReactDOM.render(
-	<Avatar user={USER_DATA}/>,
+	routes,
 	document.getElementById('app')
 );
