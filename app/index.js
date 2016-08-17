22a8df6f4b0dad5960c0d18b191d35a5cd5d82d7
Kathy H <kathykehuang@gmail.com>
2016-08-16 22:07:59 -0400

    Add router, using Sentry, PropTypes and function stateless components

--- app/index.js
+++ app/index.js
@@ -1,7 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('../config/routes');
+var Raven = require('raven-js');
 
+var sentryKey = 'cd2a3db5675a4c1e859ef3f56ba0b31b';
+var sentryApp = '92512';
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+var _APP_INFO = {
+	name: 'Github Battle',
+	branch: '4',
+	version: '1.0'
+};
+
+Raven.config(sentryURL, {
+	release: _APP_INFO.version,
+	tags: {
+		branch: _APP_INFO.branch
+	}
+}).install();
 
 ReactDOM.render(
 	routes,
